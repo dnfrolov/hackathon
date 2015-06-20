48372d2444774c03de2bb9ef604f1f7cff0e8855
AlexMatusevich <aleks_hunter@mail.ru>
2015-06-21 01:17:13 +0300

    Improved Profile page.

--- public/js/user/user.js
+++ public/js/user/user.js
@@ -4,8 +4,20 @@ var angular = require('angular');
 
 angular.module('User', [])
     .config(['$stateProvider', require('./user-config')])
-    .controller('ProfileController', ['$scope', require('./profile-controller')])
-    .controller('SaveUserController', ['$scope', '$state', 'UserService', 'user', require('./save-user-controller')])
+    .controller('ProfileController', [
+        '$scope',
+        'user',
+        'questions',
+        'responses',
+        require('./profile-controller')
+    ])
+    .controller('SaveUserController', [
+        '$scope',
+        '$state',
+        'UserService',
+        'user',
+        require('./save-user-controller')
+    ])
     .service('UserService', ['$q', '$http', 'UserModel', require('./user-service')])
     .factory('UserModel', function () {
         return require('./user-model');
